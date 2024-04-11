c3423d8e78af796c3c4675371f6dce4a092101d7
Tomer <tomermi123@gmail.com>
2024-04-11 17:58:16 +0300

    database connection with env variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from "express";
-import Logger from "@utils/logger";
-
 const app: Express = express();
-const PORT = 3000;
+import Logger from "./utils/logger";
+
+import env from 'dotenv';
+env.config();
+
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const db = mongoose.connection;
+db.on("error", (error)=> console.error(error));
+db.once("open", ()=> console.log("Connected to Database"));
+mongoose.connect(process.env.DB_URL as string);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express!");
 });
 
-app.listen(PORT, () => {
-  Logger.info("Server is running");
+app.listen(process.env.PORT, () => {
+  Logger.info(`Server is running on port ${process.env.PORT}`);
 });
