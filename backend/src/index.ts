f9d15f3c45ebba8f01e44343fba3fa9a2351133b
Tomer <tomermi123@gmail.com>
2024-04-15 00:38:31 +0300

    Refactor database connection code and add error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from "express";
-const app: Express = express();
 import Logger from "./utils/logger";
 
 import env from 'dotenv';
@@ -8,11 +7,7 @@ env.config();
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
-const db = mongoose.connection;
-db.on("error", (error)=> console.error(error));
-db.once("open", ()=> console.log("Connected to Database"));
-mongoose.connect(process.env.DB_URL as string);
-
+const app: Express = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,6 +15,16 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express!");
 });
 
-app.listen(process.env.PORT, () => {
-  Logger.info(`Server is running on port ${process.env.PORT}`);
-});
+const db = mongoose.connection;
+db.on("error", (error)=> console.error(error));
+db.once("open", ()=> console.log("Connected to Database"));
+mongoose.connect(process.env.DB_URL as string)
+  .then(() => {
+    Logger.info("Connected to Database");
+    app.listen(process.env.PORT, () => {
+      Logger.info(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    Logger.error(`Error connecting to MongoDB: ${error}`);
+  });
