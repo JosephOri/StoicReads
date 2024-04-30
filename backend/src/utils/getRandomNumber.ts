37e667f9881b53675951e06ec310cd648e2094e1
Ori <ori100no@gmail.com>
2024-04-30 15:34:04 +0300

    removed magic numbers

--- backend/src/utils/getRandomNumber.ts
+++ backend/src/utils/getRandomNumber.ts
@@ -1,9 +1,13 @@
 import crypto from 'crypto';
 
+const BYTE_SIZE = 4;
+const MAX_32BIT_INT = 0xffffffff;
+const RADIX = 16;
+
 const getRandomNumber = (min: number, max: number) => {
-  const bytes = crypto.randomBytes(4);
-  const randomNumber = parseInt(bytes.toString('hex'), 16);
-  return Math.floor(randomNumber / (0xffffffff / (max - min + 1))) + min;
+  const bytes = crypto.randomBytes(BYTE_SIZE);
+  const randomNumber = parseInt(bytes.toString('hex'), RADIX);
+  return Math.floor(randomNumber / (MAX_32BIT_INT / (max - min + 1))) + min;
 };
 
 export default getRandomNumber;
