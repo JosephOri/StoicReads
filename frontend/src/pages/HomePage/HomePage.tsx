f2300b1947b5144c0b47c5224681f979ca546bb9
Ori <ori100no@gmail.com>
2024-05-15 15:03:42 +0300

    added logout option on the `HomePage` and right now it is really ugly. I will fix it later.

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,23 @@
-
+import { Button } from "@mui/material"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN_KEY, AUTH_LOGOUT_URL, REFRESH_TOKEN_KEY } from "../../utils/constants";
+import { allowCorsForAxios } from "../../utils/allowCorsForAxios";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleLogOut = async () => {
+    localStorage.removeItem(ACCESS_TOKEN_KEY);
+    navigate("/login");
+    allowCorsForAxios(axios)
+    await axios.post(AUTH_LOGOUT_URL, { refreshToken: localStorage.getItem(REFRESH_TOKEN_KEY) });
+    localStorage.removeItem(REFRESH_TOKEN_KEY);
+  };
+
   return (
-    <div>HomePage</div>
+    <>
+      <h1>Home Page</h1>
+      <Button onClick={handleLogOut}>Log Out</Button>
+    </>
   )
 }
 
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -4,3 +4,4 @@ export const BASE_URL = 'http://localhost:3000';
 export const AUTH_URL = `${BASE_URL}/auth`;
 export const AUTH_LOGIN_URL = `${AUTH_URL}/login`;
 export const AUTH_REGISTER_URL = `${AUTH_URL}/register`;
+export const AUTH_LOGOUT_URL = `${AUTH_URL}/logout`;
