77faa32905e61dbed4ee6e14c4d200ed9333d9d1
Tomer <tomermi123@gmail.com>
2024-05-27 21:40:54 +0300

    chore: hardcode Google OAuth client ID in frontend code. env variable is unusable.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
+// const clientID : string = process.env.REACT_APP_Google_OAuth_Client_ID;
+// console.log(process.env);
+// !!! I cant use the env variables in the frontend, so I have to hardcode the clientID
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_Google_OAuth_Client_ID ?? ''}>
+  <GoogleOAuthProvider clientId='369022282703-7gjvbrufu0ge489a7uhnqugbcuk7muf5.apps.googleusercontent.com'>
       <React.StrictMode>
     <App />
   </React.StrictMode>,
