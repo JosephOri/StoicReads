ea7decbd39992b4f94de925bb0e9694342b32eab
Tomer <tomermi123@gmail.com>
2024-05-31 13:33:03 +0300

    chore: Update main.tsx to use React.StrictMode for improved error detection and warnings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_APP_Google_OAuth_Client_ID}>
-    <App />
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_APP_Google_OAuth_Client_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 )
