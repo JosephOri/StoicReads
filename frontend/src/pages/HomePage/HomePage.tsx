4dff343c16fd71d2d00253f2d20c9728990658d8
Ori <ori100no@gmail.com>
2024-07-16 13:47:47 +0300

    styled the app

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -19,11 +19,10 @@ import {
 import CloseIcon from "@mui/icons-material/Close";
 import useSWR from "swr";
 import axios from "axios";
-import { useGlobal } from "../../hooks/useGlobal";
-import { text } from "stream/consumers";
 import { POSTS_URL } from "../../utils/constants";
 import useCurrentUser from "../../hooks/useCurrentUser";
 import { useNavigate } from "react-router-dom";
+
 type Post = Record<string, unknown>;
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
@@ -33,7 +32,7 @@ const HomePage = () => {
   const [open, setOpen] = useState(false);
   const [selectedPost, setSelectedPost] = useState<Post | null>(null);
   const [newComment, setNewComment] = useState("");
-  const {user} = useCurrentUser();
+  const { user } = useCurrentUser();
   const navigate = useNavigate();
 
   const handleClickOpen = (post: Post) => {
@@ -47,13 +46,13 @@ const HomePage = () => {
     setNewComment("");
   };
 
-  const handleAddComment = async() => {
+  const handleAddComment = async () => {
     if (newComment.trim() === "") return;
     const updatedPost = {
       ...selectedPost,
       comments: [
         ...selectedPost?.comments,
-        { username:user?.userName , content: newComment },
+        { username: user?.userName, content: newComment },
       ],
     };
     await axios.put(`${POSTS_URL}/${selectedPost?._id}`, updatedPost);
@@ -61,82 +60,60 @@ const HomePage = () => {
     setNewComment("");
   };
 
-   const handleDeleteComment = async (index:number) => {
+  const handleDeleteComment = async (index: number) => {
     const isConfirmed = window.confirm("Are you sure you want to delete this comment?");
     if (!isConfirmed) return;
     const updatedPost = {
       ...selectedPost,
-      comments: selectedPost?.comments.filter((comment:any,i:number)=>i!==index),
+      comments: selectedPost?.comments.filter((comment: any, i: number) => i !== index),
     };
     await axios.put(`${POSTS_URL}/${selectedPost?._id}`, updatedPost);
     setSelectedPost(updatedPost);
-  }
+  };
 
-  const handleDeletePost = async (id:unknown) => {
+  const handleDeletePost = async (id: unknown) => {
     const isConfirmed = window.confirm("Are you sure you want to delete this post?");
     if (!isConfirmed) return;
     await axios.delete(`${POSTS_URL}/${id}`);
-  }
-
+    handleClose();
+  };
 
   if (error) return <div>Failed to load posts</div>;
   if (!posts) return <CircularProgress />;
 
   return (
-<>
-  <h1 style={{ textAlign: "center" }}>{`Welcome Back ${user?.userName}`}</h1>
-  <Grid container spacing={4} direction="row" alignItems="center" justifyContent="center" style={{ marginTop: 20 }}>
-     {posts.map((post: Post, index: number) => (
-      <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
-        <Card
-          onClick={() => handleClickOpen(post)}
-          style={{ width: "100%", height: "100%", position: "relative" }}>
-
-          {user?.userName === post?.userName && (
-            <IconButton
-              onClick={(e) => {
-                e.stopPropagation();
-                handleDeletePost(post?._id);
-              }}
-              style={{
-                position: 'absolute',
-                top: 0,
-                right: 0,
-                zIndex: 1,
-                color: 'white',
-              }}
+    <>
+      <h1 style={{ textAlign: "center" }}>{`Welcome Back ${user?.userName}`}</h1>
+      <Grid container spacing={4} direction="row" alignItems="center" justifyContent="center" style={{ marginTop: 20 }}>
+        {posts.map((post: Post, index: number) => (
+          <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
+            <Card
+              onClick={() => handleClickOpen(post)}
+              style={{ width: "100%", height: "100%", position: "relative" }}
             >
-              <CloseIcon style={{fontSize: 30}}/>
-            </IconButton>
-          )}
-
-          <CardMedia
-            component="img"
-            height="200"
-            image={post?.book.image}
-            alt={post?.book.title}
-          />
-
-        <CardContent>
-        <Typography variant="h5" component="div">
-          {post?.title}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          Posted by {post?.userName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {post?.comments?.length} Comments
-        </Typography>
-        <Rating value={post?.review.rating} readOnly />
-      </CardContent>
-
-
-        </Card>
+         
+              <CardMedia
+                component="img"
+                height="200"
+                image={post?.book.image}
+                alt={post?.book.title}
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {post?.title}
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary">
+                  Posted by {post?.userName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {post?.comments?.length} Comments
+                </Typography>
+                <Rating value={post?.review.rating} readOnly />
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-  ))}
-        
-</Grid>
-
       <Dialog
         open={open}
         onClose={handleClose}
@@ -149,38 +126,48 @@ const HomePage = () => {
           },
         }}
       >
-        
         {open && selectedPost && (
           <>
             <DialogTitle>
               {selectedPost?.book.title}
-              <IconButton
-                aria-label="close"
-                onClick={handleClose}
-                sx={{
-                  position: "absolute",
-                  right: 8,
-                  top: 8,
-                  color: (theme) => theme.palette.grey[500],
-                }}
-              >
-                <CloseIcon />
-              </IconButton>
-
-              {user?.userName===selectedPost?.userName && <Button
-                onClick={() => navigate(`/edit-post/${selectedPost._id}`)}
-                sx={{
-                  position: "absolute",
-                  right: 50,
-                  top: 8,
-                  color: (theme) => theme.palette.grey[500],
-                }}
-                variant="outlined"
-                color="primary"
-              >
-                Edit
-              </Button>}
-              
+          
+              {user?.userName === selectedPost?.userName && (
+                  <Box
+                    sx={{
+                      position: "absolute",
+                      right: 8,
+                      top: 8,
+                      display: "flex",
+                      gap: 1,
+                    }}
+                  >
+                    <Button
+                      onClick={() => navigate(`/edit-post/${selectedPost._id}`)}
+                      sx={{
+                        color: (theme) => theme.palette.grey[500],
+                      }}
+                      variant="outlined"
+                      color="primary"
+                    >
+                      Edit
+                    </Button>
+                    
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleDeletePost(selectedPost?._id);
+                      }}
+                      sx={{
+                        color: (theme) => theme.palette.grey[500],
+                      }}
+                      variant="outlined"
+                      color="error"
+                    >
+                      Delete
+                    </Button>
+                  </Box>
+                  )}                  
+            
             </DialogTitle>
             <DialogContent dividers>
               <Box display="flex" flexDirection="row" mb={2}>
@@ -191,7 +178,6 @@ const HomePage = () => {
                     style={{ maxWidth: "200px", marginRight: "20px" }}
                   />
                 </Box>
-
                 <Box flexGrow={1}>
                   <Typography variant="h6">Posted by</Typography>
                   <DialogContentText>
@@ -210,21 +196,19 @@ const HomePage = () => {
                   </DialogContentText>
                 </Box>
               </Box>
-
               <Typography variant="h6">Comments</Typography>
               {selectedPost?.comments.map((comment: any, index: number) => (
-                <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 1 }}>
+                <Box key={index} sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 1 }}>
                   <DialogContentText>
                     <strong>{comment?.username}:</strong> {comment?.content}
                   </DialogContentText>
                   {comment?.username === user?.userName && (
                     <Button onClick={() => handleDeleteComment(index)} sx={{ marginLeft: 1 }}>
-                      <CloseIcon style={{ color: 'red', fontSize: 24 }} />
+                      <CloseIcon style={{ color: "red", fontSize: 24 }} />
                     </Button>
                   )}
                 </Box>
               ))}
-
               <Box mt={2}>
                 <TextField
                   label="Add a comment"
@@ -245,8 +229,8 @@ const HomePage = () => {
             </DialogContent>
           </>
         )}
-    </Dialog>
-</>
+      </Dialog>
+    </>
   );
 };
 
