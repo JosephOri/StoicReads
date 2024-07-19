6b9e322c74eef3bf0c2128aebd4d442703162a3c
Tomer <tomermi123@gmail.com>
2024-07-19 18:12:43 +0300

    chore: Update ProfilePage with imports, use state and form data. Intial handler logic.

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,28 +1,83 @@
-import { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  IconButton,
-  Box,
-  CircularProgress,
-  TextField,
-  Button,
-  Rating,
-} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import useSWR from "swr";
-import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios, { AxiosError, HttpStatusCode } from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import SignupFormData from "../../interfaces/SignupFormData";
+import isFormDataValidCheck from "../../utils/isFormDataValidCheck";
+import { applicationRoutes } from "../../utils/constants"
+import { useGlobal } from "../../hooks/useGlobal";
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+import {Card, CardContent, CardMedia, Grid, Typography, Box, Button }  from "@mui/material";
+  import defaultImage from '../../assets/image.jpg';
+
 
 const ProfilePage = () => {
+    const [profileData, setProfileData] = useState<SignupFormData>({
+        userName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        profileImage: defaultImage,
+    });
+    const navigate = useNavigate();
+
+    const handleInputChange = (fieldName: string, value: string) => {
+        setProfileData({ ...profileData, [fieldName]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // const isFormDataValid = isFormDataValidCheck(profileData);
+        // if (isFormDataValid){
+        //     toast.success("Form data is valid " + isFormDataValid);
+        // } else {
+        //     toast.error("Form data is invalid " + isFormDataValid);
+        //     return;
+        // }
+
+        // const {userName, email, password, profileImage} = profileData;
+        try{
+            // const response = await axios.post(applicationRoutes.USER, {
+            //     userName,
+            //     email,
+            //     password,
+            //     profileImage,
+            // });
+            // console.log(response);
+            const {user} = useGlobal();
+            console.log(user);
+            toast.success("User fetched successfully");
+
+        } catch (err: unknown) {
+            const error = err as AxiosError;
+            if (error.response?.status === HttpStatusCode.BadRequest) {
+                toast.error("Please fill in all fields");
+            } else if (error.response?.status === HttpStatusCode.Conflict) {
+                toast.error("User already exists");
+            } else {
+                toast.error("An error occurred. Please try again later");
+            }
+        }
+
+    };
+
+    const fetchUser = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get(applicationRoutes.USER);
+            console.log(response);
+        } catch (err: unknown) {
+            const error = err as AxiosError;
+            if (error.response?.status === HttpStatusCode.BadRequest) {
+                toast.error("Please fill in all fields");
+            } else if (error.response?.status === HttpStatusCode.Conflict) {
+                toast.error("User already exists");
+            } else {
+                toast.error("An error occurred. Please try again later");
+            }
+        }
+    }
+
 
 
     return (
@@ -36,16 +91,16 @@ const ProfilePage = () => {
                 style={{ marginTop: "20px" }}
             >
 
-                <Grid item>
+                <Grid item xs={12}>
                     <Typography variant="h4">Profile</Typography>
                 </Grid>
-                {/* items for displaying: username, email, profileImage and createdAt */}
-                <Grid item>
+
+                <Grid item xs={12}>
                     <Card>
                         <CardMedia
                             component="img"
                             height="140"
-                            image="https://www.w3schools.com/howto/img_avatar.png"
+                            image= {profileData.profileImage}
                             alt="green iguana"
                         />
                         <CardContent>
@@ -55,7 +110,23 @@ const ProfilePage = () => {
                         </CardContent>
                     </Card>
                 </Grid>
+
+                <Box
+                    component="form"
+                    noValidate
+                    onSubmit={fetchUser}
+                    sx={{ mt: 3 }} 
+                    >
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            sx={{ mt: 3, mb: 2 }} >
+                                Edit Profile
+                        </Button>
+                </Box>
             </Grid>
+            <ToastContainer />
         </>
     );
 };
