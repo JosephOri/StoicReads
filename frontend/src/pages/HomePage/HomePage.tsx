6f4e3bf2fbea4f34f61bdde719c87d81c85fce8c
Tomer <tomermi123@gmail.com>
2024-07-30 18:49:58 +0300

    chore: Update homepage to show message when user has no posts and improve error handling and loading states in post controller and homepage

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -64,7 +64,6 @@ const HomePage = () => {
         { username: user?.userName, content: newComment },
       ],
     };
-    console.log("Updated Post:", updatedPost);
     await axios.put(`${POSTS_URL}/${selectedPost?._id}`, updatedPost);
     setSelectedPost(updatedPost as Post);
     setNewComment("");
@@ -132,37 +131,40 @@ const HomePage = () => {
         alignItems="center" 
         justifyContent="center" 
         style={{ marginTop: 20 }}>
-        {posts.map((post: Post, index: number) => {
-          const imageUrl = post?.image ? `${BACKEND_URL}${post.image}` : post?.book.image;
-
-          return (
-            <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
-              <Card
-                onClick={() => handleClickOpen(post)}
-                style={{ width: "100%", height: "100%", position: "relative" }}
-              >
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={post?.book.image}
-                  alt={post?.book.title}
-                />
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {post?.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary">
-                    Posted by <strong>{post?.userName}</strong>
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {post?.comments?.length} Comments
-                  </Typography>
-                  <Rating value={Number(post?.review.rating)} readOnly />
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
+        {posts.length === 0 ? (
+          <Typography variant="h6">No posts available</Typography>
+        ) : (
+          posts.map((post: Post, index: number) => {
+            const imageUrl = post?.image ? `${BACKEND_URL}${post.image}` : post?.book.image;
+            return (
+              <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
+                <Card
+                  onClick={() => handleClickOpen(post)}
+                  style={{ width: "100%", height: "100%", position: "relative" }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={post?.book.image}
+                    alt={post?.book.title}
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {post?.title}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary">
+                      Posted by <strong>{post?.userName}</strong>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {post?.comments?.length} Comments
+                    </Typography>
+                    <Rating value={Number(post?.review.rating)} readOnly />
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })
+        )}
       </Grid>
       )}
       <Dialog
@@ -250,7 +252,7 @@ const HomePage = () => {
                     Review
                   </Typography>
                   <Box display="flex" alignItems="center" mb={2}>
-                    <Rating value={selectedPost?.review.rating} readOnly />
+                    <Rating value={Number(selectedPost?.review.rating)} readOnly />
                     <DialogContentText fontSize="1.25rem" ml={1}>
                       {selectedPost?.review.rating}
                     </DialogContentText>
