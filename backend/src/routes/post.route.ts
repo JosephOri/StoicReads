fa19881d75b3fcc01b5b394ac13d6f4fc919f187
Tomer <tomermi123@gmail.com>
2024-07-30 18:43:55 +0300

    chore: Update post route, add constants messages

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -6,10 +6,11 @@ const router = express.Router();
 
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
-router.get('/user/:userName', postController.getPostsByUser);
+router.get('/user/:userName', postController.getPostsByUserName);
 router.post('/', upload.single('image'), postController.createPost);
 router.put('/:id', upload.single('image'), postController.updatePost);
 router.delete('/:id', postController.deletePost);
 router.get('/uploads/:filename', postController.getImage);
+router.get('/hasUserPosts/:userName', postController.hasUserPosts);
 
 export default router;
